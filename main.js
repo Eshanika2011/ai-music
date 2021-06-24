8b6d1153e27b67340da75ffee7e5e6c4efb423a3
Eshanika2011 <75015294+Eshanika2011@users.noreply.github.com>
2021-06-24 17:27:19 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -21,16 +21,18 @@ function setup() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
 
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
-        console.log('rightWristX :'+rightWristX);
-        console.log('rightWristy :'+rightWristY);
-        
-        console.log('leftWristX :'+leftWristX);
-        console.log('leftWristY :'+leftWristY);
+        console.log('rightWristX :' + rightWristX);
+        console.log('rightWristy :' + rightWristY);
+
+        console.log('leftWristX :' + leftWristX);
+        console.log('leftWristY :' + leftWristY);
 
 
     }
@@ -42,6 +44,20 @@ function modelLoaded() {
 
 function draw() {
     image(v1, 0, 0, 800, 500)
+    fill("red");
+    stroke("red");
+    if (scoreLeftWrist>0.2) {
+        
+    
+    circle(leftWristX, leftWristY, 20);
+    number_leftWristY = Number(leftWristY);
+    remove_decimalls = floor(number_leftWristYC)
+    console.log(remove_decimalls);
+    volume = remove_decimalls / 500;
+    document.getElementById("volume").innerHTML = "Volume=" + volume;
+    song.setVolume(volume);
+}
+
 }
 
 function play() {
